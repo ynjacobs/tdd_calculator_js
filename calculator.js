299d263303983f8766145c285c77fe1617a2a3ce
ynjacobs <ytj64611@gamil.com>
2019-07-02 13:49:22 -0400

    does some calculator functions like add subtract and reduce

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
-function add() {
+function add(num1, num2) {
+    num = num1 + num2;
+    return num
 }
 
-function subtract() {
+function subtract(num1, num2) {
+    num = num1 - num2;
+    return num
+
 }
 
-function sum() {
+function sum(anArray) {
+    myArray = anArray.reduce((a, b) => a + b, 0);
+    return myArray
+    
 }
 
 module.exports = {add, subtract, sum}
